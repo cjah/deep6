2c10eb976170ef84fa02be14bd391c9e8cb94184
cjah <curtis.jah@gmail.com>
2019-03-21 17:12:34 -0700

    bootstrap css works and navbr turns into hamburgar when resized smaller

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,26 @@
 import React, { Fragment, Component } from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, Button, NavDropdown } from 'react-bootstrap';
 import styled from 'styled-components';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './bootstrap-overrides.css';
 import './Dashboard.css';
+
+
+
 import MovieRental from './MovieRental.js'
+
+const MovieButton = styled.button`
+  text-transform: uppercase;
+  color: #fff;
+  border-color: #ff4409;
+  background-color: #ff5722;
+  display: block;
+  margin: 0 auto;
+  width: 230px;
+  font-size: none;
+  line-height: normal;
+`
 
 export default class Dashboard extends Component {
   constructor(props) {
@@ -28,7 +45,7 @@ export default class Dashboard extends Component {
 
   createButton(movieIndex) {
     return (
-      <Button onClick={() => this.handleReturnButton(movieIndex)} style={{ cursor: movieIndex === this.state.hoverStateIndex ? 'pointer' : 'none' }}>Return</Button>
+      <MovieButton onClick={() => this.handleReturnMovieButton(movieIndex)} style={{ cursor: movieIndex === this.state.hoverStateIndex ? 'pointer' : 'none' }}>Return</MovieButton>
     )
   }
 
@@ -37,21 +54,63 @@ export default class Dashboard extends Component {
   }
 
   render() {
+  //   <Navbar id="navbar" collapseOnSelect expand='sm' bg="dark" variant="dark">
+  //   <Navbar.Brand>
+  //     <img src={this.props.image.src} alt={this.props.image.alt} className="logo"/>
+  //   </Navbar.Brand>
+  //   <Navbar.Collapse id="responsive-navbar-nav">
+  //     <Nav className="tabs">
+  //       <Nav.Link>Movies</Nav.Link>
+  //       <Nav.Link>TV Shows</Nav.Link> 
+  //     </Nav>
+  //   </Navbar.Collapse>
+
+  // </Navbar>
+
+/* <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar> */
+
 
     return (
       <div id="dashboardRoot">
-        <Navbar id="navbar" bg="dark" variant="dark">
+
+        <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
           <Navbar.Brand>
             <img src={this.props.image.src} alt={this.props.image.alt} className="logo"/>
           </Navbar.Brand>
-          <Nav className="tabs">
-            <Nav.Link>Movies</Nav.Link>
-            <Nav.Link>TV Shows</Nav.Link> 
-          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link>Movies</Nav.Link>
+              <Nav.Link>TV Shows</Nav.Link> 
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
-        <div
-          id="dashboard"
-        >
+
+        
+
+        <div id="dashboard">
 
           {
             this.props.movies.map((movie, i) => {
